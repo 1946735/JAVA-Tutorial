aa59231fa320209b7476b5e033e3ade1510d5f68
19467 <19467@NOI>
2021-06-09 21:55:26 +0900

    ㅠㅠ

--- Ex6/CallStackTest2.java
+++ Ex6/CallStackTest2.java
@@ -242,3 +242,40 @@ System.out.println(mm.divide());
 
 }
 }
+
+class MemberCall {
+	
+//	System.out.println("page. 280 : 메소드의 범위 : 클래스메서드 > 인스턴스메서드");
+	
+	int iv = 10;
+	static int cv = 20; 
+	
+	int iv2 = cv;
+//	static int cv2 = iv; 에러. 클래스변수는 인스턴스변수를 사용할 수 없음.
+	static int cv2 = new MemberCall().iv; // 객체 생성 후 사용가능
+	
+	static void staticMethod1() {
+		System.out.println(cv);
+//		System.out.println(iv); 에러. 클래스매서드이기 때문에 인스턴스변수를 사용할 수 없음.
+		MemberCall c = new MemberCall();; // 객체생성
+		System.out.println(c.iv); // 객체를 생성한 후 인스턴스변수의 참조 가능.
+	}
+	
+	void instanceMethod1() {
+		System.out.println(cv);
+		System.out.println(iv); // 인스턴스메서드에서 인스턴스변수를 바로 사용 가능함.
+	}
+	
+	static void staticMethod2() {
+		staticMethod1();
+//		instanceMethod1(); 에러 . 클래스메서드에서는 인스턴스메서드를 호출 할 수 없음.
+		MemberCall c = new MemberCall();
+		c.instanceMethod1(); // 인스턴스메서드를 생성 해야 호출 할 수 있음.
+		
+	}
+	
+	void instanceMethod2() { //인스턴스메서드에서 인스턴스메서드와 클래스메서드
+		staticMethod1(); 
+		instanceMethod1();
+	}
+}
--- Ex6/Overloading.java
+++ Ex6/Overloading.java
@@ -1,5 +1,9 @@
 package Ex6;
 
+/**
+ * @author 19467
+ *
+ */
 public class Overloading {
 
 	public static void main(String[] args) {
@@ -78,3 +82,35 @@ class VarArgsEx {
 
 */
 } // class
+
+class Car {
+	String color;
+	String gearType;
+	int door;
+	
+	Car() {}
+	
+	Car(String c, String g, int d) {
+		color = c;
+		gearType = g;
+		door = d;
+	}
+}
+
+class CarTest{
+	public static void main (String[] args) {
+		Car c1 = new Car();
+		
+		c1.color = "white";
+		c1.gearType = "auto";
+		c1.door = 5;
+		
+		Car c2 = new Car("white", "auto", 4);
+		System.out.println("c1ÀÇ color=" + c1.color + ", gearType="+c1.gearType+ ", door="+c1.door);
+		System.out.println("c2ÀÇ color=" + c2.color + ", gearType="+c2.gearType+ ", door="+c2.door);
+	}
+}
+
+
+
+
